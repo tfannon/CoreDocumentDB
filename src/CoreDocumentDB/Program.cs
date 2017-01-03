a4d7ca53db21fad235bd779535f26872fd77484a
tfannon <tfannon@gmail.com>
2017-01-03 00:06:57 -0500

    document db initial

--- src/CoreDocumentDB/Program.cs
+++ src/CoreDocumentDB/Program.cs
@@ -2,13 +2,151 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
 
 namespace CoreDocumentDB
 {
     public class Program
     {
+        private const string EndpointUri = "https://tommywk.documents.azure.com:443/";
+        private const string PrimaryKey = "9q1yPRG14f5Fs6MpRWhra33EpBYxgTCvkOHXv2zsEEQ7aucZWf6M5rAX49XVprDXRRsOXK9b4eUMoxyI94250w==";
+        private DocumentClient client;
         public static void Main(string[] args)
         {
+            try
+            {
+                var p = new Program();
+                p.GetStartedDemo().Wait();
+            }
+            catch (DocumentClientException de)
+            {
+                var baseException = de.GetBaseException();
+                Console.WriteLine("{0} error occurred: {1}, Message: {2}", de.StatusCode, de.Message, baseException.Message);
+            }
+            catch (Exception e)
+            {
+                var baseException = e.GetBaseException();
+                Console.WriteLine("Error: {0}, Message: {1}", e.Message, baseException.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of demo, press any key to exit.");
+                Console.ReadKey();
+            }
         }
+
+        private async Task GetStartedDemo()
+        {
+            client = new DocumentClient(new Uri(EndpointUri), PrimaryKey);
+            await client.CreateDatabaseIfNotExistsAsync(new Database { Id = "FamilyDB_oa" });
+            await client.CreateDocumentCollectionIfNotExistsAsync("dbs/FamilyDB_oa",
+                    new DocumentCollection {Id = "FamilyCollection_oa" });
+
+            var andersenFamily = new Family
+            {
+                Id = "Andersen.1",
+                LastName = "Andersen",
+                Parents = new[]
+                {
+                    new Parent { FirstName = "Thomas" },
+                    new Parent { FirstName = "Mary Kay" }
+                },
+                Children = new[] 
+                {
+                    new Child
+                    {
+                            FirstName = "Henriette Thaulow",
+                            Gender = "female",
+                            Grade = 5,
+                            Pets = new[]
+                            {
+                                new Pet { GivenName = "Fluffy" }
+                            }
+                    }
+                },
+
+                Address = new Address { State = "WA", County = "King", City = "Seattle" },
+                IsRegistered = true
+            };
+
+            await this.CreateFamilyDocumentIfNotExists("FamilyDB_oa", "FamilyCollection_oa", andersenFamily);
+        }
+
+        // ADD THIS PART TO YOUR CODE
+        private async Task CreateFamilyDocumentIfNotExists(string databaseName, string collectionName, Family family)
+        {
+            try
+            {
+                await this.client.ReadDocumentAsync(UriFactory.CreateDocumentUri(databaseName, collectionName, family.Id));
+                this.WriteToConsoleAndPromptToContinue("Found {0}", family.Id);
+            }
+            catch (DocumentClientException de)
+            {
+                if (de.StatusCode == HttpStatusCode.NotFound)
+                {
+                    await this.client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(databaseName, collectionName), family);
+                    this.WriteToConsoleAndPromptToContinue("Created Family {0}", family.Id);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+
+        private void WriteToConsoleAndPromptToContinue(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
+        }
+
+        // ADD THIS PART TO YOUR CODE
+        public class Family
+        {
+            [JsonProperty(PropertyName = "id")]
+            public string Id { get; set; }
+            public string LastName { get; set; }
+            public Parent[] Parents { get; set; }
+            public Child[] Children { get; set; }
+            public Address Address { get; set; }
+            public bool IsRegistered { get; set; }
+            public override string ToString()
+            {
+                return JsonConvert.SerializeObject(this);
+            }
+        }
+
+        public class Parent
+        {
+            public string FamilyName { get; set; }
+            public string FirstName { get; set; }
+        }
+
+        public class Child
+        {
+            public string FamilyName { get; set; }
+            public string FirstName { get; set; }
+            public string Gender { get; set; }
+            public int Grade { get; set; }
+            public Pet[] Pets { get; set; }
+        }
+
+        public class Pet
+        {
+            public string GivenName { get; set; }
+        }
+
+        public class Address
+        {
+            public string State { get; set; }
+            public string County { get; set; }
+            public string City { get; set; }
+        }
+
     }
 }
